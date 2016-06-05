fc9c06a4a461b79b2568ae8b2119fc0d064dbad8
Bruno Furtado <brunofurtadoc@gmail.com>
2016-06-05 20:23:44 -0300

    Create the project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name openlayrFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the openlayrFrontendApp
+ */
+angular.module('openlayrFrontendApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
